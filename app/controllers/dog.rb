b992b690449fd14e56cc93cb2362f4e32ee23701
Ming Duan <11203duan@gmail.com>
2019-05-20 17:56:00 +0000

    Automatically backed up by Learn

--- app/controllers/dog.rb
+++ app/controllers/dog.rb
@@ -3,7 +3,7 @@ class Dog < ApplicationController
   attr_accessor :name, :age
   attr_reader :breed
 
-  def initialization(name=nil, breed, age)
+  def initialization(name=nil, breed=nil, age=nil)
     @name = name
     @breed = breed
     @age = age
